783a7d42fb6beca6a2399b1e45f51d04d7ada5e4
Cesarin43 <ceanguti2005@gmail.com>
2024-10-12 17:19:46 -0500

    Se corrigió error de libreria en el componente de administrador y se conectó el inventario a la base de datos

--- src/components/Administrador.jsx
+++ src/components/Administrador.jsx
@@ -1,29 +1,50 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom"; // Importa useNavigate
 import "../App.css";
 
-const Administrador = () =>{
-    return (
-        <div className="administrador-page">
-            <header>
-                <nav>
-                    <ul>
-                        <li><link to="/productos">Ver Productos</link></li>
-                        <li><link to="/Login">Cerrar Sesión</link></li>
-                    </ul>
-                </nav>
-                <h1>Mandy Snap</h1>
-            </header>
-            <section>
-                <h2>Administrador</h2>
-                <div className="buttons">
-                    <button>Gastos</button>
-                    <button>Ventas</button>
-                    <button>Ingresos</button>
-                </div>
-            </section>
+const Administrador = () => {
+  const navigate = useNavigate(); // Define el hook de navegación
+
+  const handleVerProductos = () => {
+    navigate("/productos");
+  };
+
+  const handleCerrarSesion = () => {
+    navigate("/login");
+  };
+
+  const handleGastosClick = () => {
+    navigate("/gastos");
+  };
+  const handleVentasClick = () => {
+    navigate("/ventas");
+  };
+
+  return (
+    <div className="administrador-page">
+      <header>
+        <nav>
+          <ul>
+            <li>
+              <button onClick={handleVerProductos}>Ver Productos</button>
+            </li>
+            <li>
+              <button onClick={handleCerrarSesion}>Cerrar Sesión</button>
+            </li>
+          </ul>
+        </nav>
+        <h1>Mandy Snap</h1>
+      </header>
+      <section>
+        <h2>Administrador</h2>
+        <div className="buttons">
+          <button onClick={handleGastosClick}>Gastos</button>
+          <button onClick={handleVentasClick}>Ventas</button>
+          <button>Ingresos</button>
         </div>
-    );
+      </section>
+    </div>
+  );
 };
 
 export default Administrador;
